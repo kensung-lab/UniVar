30152db950ab55cdaa383d0f708c97fa5d697b14
victoryangHKGI <vytyang@genomics.org.hk>
2024-11-27 15:50:53 +0800

    add listener

---
+++ variant-import-tool/listen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set the directory to monitor
+MONITOR_DIR=./data/control_files
+
+# Monitor new folder using inotifywait
+echo "$(date): Starting to monitor $MONITOR_DIR for new files..."
+./inotify/bin/inotifywait -m -e create --format '%f' "$MONITOR_DIR" | while read -r folder_name; do
+
+  echo "Detected folder: $folder_name, wait for 2 seconds..."
+  sleep 2
+  echo "Start processing folder: $folder_name"
+
+  # Iterate control files in the new folder
+  folder_path="$MONITOR_DIR/$folder_name"
+  find "$folder_path" -type f | while read -r filepath; do
+
+    filename=$(basename "$filepath")
+    if [ "$filename" == "snp.json" ]; then
+      echo "Run SNP: $filepath"
+      ./run_snp.sh "$filepath"
+    else
+      echo "Run SV: $filepath"
+      ./run_sv.sh "$filepath"
+    fi
+  done
+
+  echo "Run Complete"
+  ./complete.sh "$filepath"
+
+done
